1b4928407366077b9c84bbdcf92ff33d0fc1ee5a
AubreyJohns <aubreyomondi@gmail.com>
2020-12-01 16:30:21 +0300

    remove useless comments

--- soap/soap/soap/urls.py
+++ soap/soap/soap/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'soap.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^soap_service/', 'soap_students_app.views.my_soap_application'),
 ]
--- soap/soap_students_client.py
+++ soap/soap_students_client.py
@@ -3,4 +3,4 @@ from suds.cache import NoCache
 
 my_client = Client('http://127.0.0.1:8000/soap_service/?WSDL', cache=NoCache())
 print my_client
-print 'Student details:\n', my_client.service.student_details(654321)
+print 'Student details:\n', my_client.service.student_details(101907)
